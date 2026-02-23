20d3b8807015c9ff51bd1c60617ccfdfb9dd91c6
emtee-1023 <marktalamson@gmail.com>
2026-02-23 17:03:15 +0300

    stage

--- app/Http/Controllers/ConfigController.php
+++ app/Http/Controllers/ConfigController.php
@@ -51,6 +51,7 @@ class ConfigController extends Controller
             'company_id' => $company->company_id,
             'app_name' => $app->app->app_name,
             'app_id' => $app->app_id,
+            'company_db_id' => $company->id,
             'branding' => [
                 'company_name' => $company->name,
                 'logo_url' => $app->branding['logo_url'] ?? null,
